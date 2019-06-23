b1483aa6db0ec97f7f82e3387596e196d8607008
Joseph Scott <jbs198585@gmail.com>
2019-06-23 10:10:30 -0400

    solution

---
+++ annoy.js
@@ -0,0 +1,12 @@
+var answer = prompt("Are we there yet?");
+
+while(answer !== "yes" && answer !== "yeah") {
+	var answer = prompt("Are we there yet?");
+}
+
+alert("YAY, we made it!");
+
+// if(answer === "yes") {
+// 	alert("YAY, we made it!");
+// } else {
+// }
